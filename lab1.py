b0a7c9071ba2d86cfadfdd0f7a518bdc7a09100e
pankaj114425 <115251359+pankaj114425@users.noreply.github.com>
2023-01-23 12:21:04 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,15 +1,33 @@
-def reverseword(str):
-     i=1
-     j=len(str) - 2   
-                                   
-     while(i<j):
-        temp=str[i]
-        str[i]=str[j]
-        str[j]=temp
-        i +=1
-        j -=1
+psss=[".",",","?","!"]
 
-     return "".join(str)
+def reverseword(str):
+    l=len(str)
+    i=1
+    j=len(str) - 3  
+    k=j-1 
+    if str[l-1] in psss:
+
+
+
+
+        while(i<j):
+
+
+
+            temp=str[i]
+            str[i]=str[j]
+            str[j]=temp
+            i +=1
+            j -=1
+        return "".join(str)
+    else:
+        while(i<k):
+            temp1=str[i]  
+            str[i]=str[k]
+            str[k]=temp1
+            i +=1
+            k-=1
+        return "".join(str)    
 def reversewords(str):
     str=str.split()
 
@@ -18,7 +36,7 @@ def reversewords(str):
                 # print (j)
         print(reverseword(j), end= "  ")
 str=input("enter string ")
-reversewords(str)  
+reversewords(str) 
 
 
 

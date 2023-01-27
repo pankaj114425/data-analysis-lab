250d76e81db01a389dedf660071c8af4bb054041
pankaj114425 <115251359+pankaj114425@users.noreply.github.com>
2023-01-27 14:35:37 +0530

    Update exercise3_5.py

--- exercise3_5.py
+++ exercise3_5.py
@@ -1,1 +1,11 @@
 
+combinations = {}
+for i in range(1, 7):
+    for j in range(1, 7):
+        sum_of_dice = i + j
+        if sum_of_dice not in combinations:
+            combinations[sum_of_dice]=1
+
+        else:
+            combinations[sum_of_dice]+=1
+print(combinations)
